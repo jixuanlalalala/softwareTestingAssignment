db3d371578b6765e8d73546a2d9c3713c672ac55
jixuanlalalala <jixuan09@1utar.my>
2025-08-26 09:05:45 +0800

    getPassengerTypeDiscount() wrote, the other method update later

--- UECS2354_GroupNumber_06/src/main/java/my/edu/utar/applyDiscountSurcharge.java
+++ UECS2354_GroupNumber_06/src/main/java/my/edu/utar/applyDiscountSurcharge.java
@@ -2,4 +2,36 @@ package my.edu.utar;
 
 public class applyDiscountSurcharge {
 
+	public static double getPassengerTypeDiscount(String passengerType, double distance) {
+		if (passengerType == "Adult") {
+			return 1.0;
+		} else if (passengerType == "Senior citizen") {
+			return 0.5;
+		} else if (passengerType == "Student") {
+			return 0.7;
+		} else if (passengerType == "Child") {
+			if (distance < 5) {
+				return 0;
+			} else {
+				return 0.5;
+			}
+		} else {
+			throw new IllegalArgumentException("Passenger type is undefined");
+		}
+	}
+	
+	public static double getTimeDateDiscount() {
+		return 1.0;
+		// update later
+	}
+	
+	public static int getFlatSurcharge() {
+		return 1;
+		//update later
+	}
+	
+	public static double getTotalDiscount() {
+		return 1.0;
+		//update later
+	}
 }
--- UECS2354_GroupNumber_06/src/main/java/my/edu/utar/paymentMethodAdjustment.java
+++ UECS2354_GroupNumber_06/src/main/java/my/edu/utar/paymentMethodAdjustment.java
@@ -1,5 +1,14 @@
 package my.edu.utar;
 
 public class paymentMethodAdjustment {
-
+	
+	public static double getPaymentMethodDiscount() {
+		return 1.0;
+		//update later
+	}
+	
+	public static double applyPaymentDiscount() {
+		return 1.0;
+		//update later
+	}
 }
