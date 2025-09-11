eedd7f0edee25c93cff841a18da2a15f79ab5c8c
chunhei <chlai6232005@gmail.com>
2025-09-12 07:02:16 +0800

    calculate Fare update

--- UECS2354_GroupNumber_06/src/test/java/my/edu/utar/FileUtilitiesUnitTest.java
+++ UECS2354_GroupNumber_06/src/test/java/my/edu/utar/FileUtilitiesUnitTest.java
@@ -11,8 +11,9 @@ import junitparams.Parameters;
 @RunWith(JUnitParamsRunner.class)
 public class FileUtilitiesUnitTest {
 
-	String filePath = "dummyfile1.txt";
-
+	String filePath = "dummyfile.txt";
+	
+	FileUtilities fu = new FileUtilities();
 	
 	private Object[] getParamsForTestReadAndWrite() {
         return new Object[]{
@@ -27,28 +28,25 @@ public class FileUtilitiesUnitTest {
 	@Parameters(method = "getParamsForTestReadAndWrite")
 	public void testReadAndWrite(String[] input) {
 		
-		FileUtilities fu = new FileUtilities();
-		
 		// Write test data
         fu.writeStringsToFile(input, filePath);
 
         // Read back
         String[] stringsFromFile = fu.readStringsFromFile(filePath);
-		assertArrayEquals(stringsFromFile, input);
+        assertArrayEquals(input, stringsFromFile);
 	}
 	
 	
 	private Object[] getParamsForTestReadWithError() {
         return new Object[]{
-            new Object[]{"somecrazyfilenamethatdoesnotexist.txt"},
-            new Object[]{"another_non_existent_file.xyz"}
+            new Object[]{"somecrazyfilenamethatdoesnotexist.txt"}
         };
     }
 
 	@Test(expected=IllegalArgumentException.class)	
 	@Parameters(method = "getParamsForTestReadWithError")
 	public void testReadWithError(String invalidFilePath) {
-		FileUtilities fu = new FileUtilities();
+		
 		fu.readStringsFromFile(invalidFilePath);
 	}
 
